e12e9decddd7fb93753ab39f04cc58d378d66f85
cbping-pc <452775680@qq.com>
2017-05-27 17:23:03 +0800

    Simple text input

--- js/widget/text.js
+++ js/widget/text.js
@@ -1,4 +1,4 @@
-(function ($widget,$util) {
+(function ($widget, $util) {
     'use strict';
     var CrysyanTextWidget = $widget.clone();
 
@@ -29,7 +29,7 @@
         Text.saveDrawingSurface();
         Text.restoreDrawingSurface();
         Text.textCursor.draw(loc.x, loc.y);
-        Text.textInput.draw(e.clientX,e.clientY);
+        Text.textInput.draw(e.clientX, e.clientY);
         Text.textCursor.blinkCursor(loc.x, loc.y);
     };
 
@@ -94,8 +94,8 @@
                 this.width, this.getHeight(context));
         },
 
-        clear:function () {
-            this.blinking=false;
+        clear: function () {
+            this.blinking = false;
             clearInterval(this.blinkingInterval);
             this.erase();
         },
@@ -108,7 +108,7 @@
             this.blinkingInterval = setInterval(function (e) {
                 cursor.erase();
                 setTimeout(function (e) {
-                    if (!cursor.blinking){
+                    if (!cursor.blinking) {
                         cursor.erase();
                         return;
                     }
@@ -123,48 +123,69 @@
 
 // TextInput.........................................................
 
-    var TextInput=function (text,width) {
-        this.text=text;
+    var TextInput = function (text, width) {
+        this.text = text;
         this.context = text.crysyanCanvas.playContext;
-        this.left = 0;
-        this.top = 0;
+        this.textLoc = null;
         this.width = width || 30;
-        this.fontSize="";
-        this.fontStyle="";
-        this.style="position:fixed;z-index:999;font-weight: bold;";
-        this.element=null;
-        this.canvasPosition=text.crysyanCanvas.canvasPosition();
+        this.fontSize = "";
+        this.fontStyle = "";
+        this.style = "position:fixed;z-index:999;font-weight: bold;";
+        this.element = null;
+        this.canvasPosition = text.crysyanCanvas.canvasPosition();
     };
 
-    TextInput.prototype={
-        init:function () {
-            this.element=document.createElement("textarea");
-            this.element.style=this.style;
-            this.element.style.display="none";
-            var textarea=this.element;
-            var textInput=this;
+    TextInput.prototype = {
+        init: function () {
+            this.element = document.createElement("textarea");
+            this.element.style = this.style;
+            this.element.style.display = "none";
+            var textArea = this.element;
+            var textInput = this;
             (document.getElementsByTagName("body").item(0) || document.documentElement).appendChild(this.element);
-            this.element.onresize=function () {
-                console.dir(textarea.style);
-                var style=textarea.style;
-                if(style.left+style.width>textInput.canvasPosition.bottom){
-                    style.width=textInput.canvasPosition.bottom-style.left;
+            this.element.onresize = function () {
+                console.dir(textArea.style);
+                var style = textArea.style;
+                if (style.left + style.width > textInput.canvasPosition.bottom) {
+                    style.width = textInput.canvasPosition.bottom - style.left;
+                }
+            };
+
+            this.element.onkeydown = function (e) {
+                var theEvent = e || window.event;
+                var code = theEvent.keyCode || theEvent.which || theEvent.charCode;
+                if (code === 13) {
+                    //回车执行
+                    textInput.text.textCursor.clear();
+                    var wordWidth = textInput.context.measureText('你').width;
+                    var lineSize = textArea.clientWidth / wordWidth;
+                    var y = textInput.textLoc.y;
+                    for (var index = 0; index < textArea.value.length;) {
+                        textInput.context.fillText(textArea.value.substr(index * lineSize, lineSize), textInput.textLoc.x, y, textArea.clientWidth);
+                        index += 1;
+                        y += wordWidth + wordWidth / 6;
+                    }
+                    textInput.text.textInput.clear();
                 }
             };
         },
-        draw:function (left, top) {
-            if (!this.element){
+        draw: function (left, top) {
+            if (!this.element) {
                 this.init();
             }
-            var style=this.element.style;
-            style.display="inline";
-            style.left=left+"px";
-            style.top=top+"px";
+            left += this.text.textCursor.width;
+            this.textLoc = this.text.crysyanCanvas.windowToCanvas(left, top);
+            var style = this.element.style;
+            style.display = "inline";
+            // Not cover Cursor
+            style.left = left + 4 + "px";
+            style.top = top + 4 + "px";
+            this.element.focus();
         },
-        clear:function () {
-            if (this.element){
-                this.element.parentNode&&this.element.parentNode.removeChild(this.element);
-                this.element=null;
+        clear: function () {
+            if (this.element) {
+                this.element.parentNode && this.element.parentNode.removeChild(this.element);
+                this.element = null;
             }
         }
 
@@ -173,4 +194,4 @@
     CrysyanTextWidget.CrysyanWidgetType = "CrysyanTextWidget";
     // export to window
     window.CrysyanTextWidget = CrysyanTextWidget;
-})(CrysyanWidget,CrysyanUtil);
+})(CrysyanWidget, CrysyanUtil);
